4390ea3fd86c5e4786927997b565b2cf2e30658e
abhishek sankpal <abhishek.sankpal12@gmail.com>
2024-02-28 16:42:23 +0530

    add logger

--- src/config/logger.ts
+++ src/config/logger.ts
@@ -3,15 +3,38 @@ import winston from 'winston';
 const logger = winston.createLogger({
     level: 'info',
     defaultMeta: {
-        serviceName: 'BLINK',
+        service: 'blink',
     },
     transports: [
         new winston.transports.File({
             dirname: 'logs',
-            filename: 'error.log',
+            filename: 'combine.log',
             level: 'info',
+            format: winston.format.combine(
+                winston.format.timestamp(),
+                winston.format.json(),
+            ),
+        }),
+
+        new winston.transports.File({
+            dirname: 'logs',
+            filename: 'error.log',
+            level: 'error',
+            format: winston.format.combine(
+                winston.format.timestamp(),
+                winston.format.json(),
+            ),
+        }),
+
+        new winston.transports.Console({
+            level: 'info',
+
+            format: winston.format.combine(
+                winston.format.timestamp(),
+                winston.format.json(),
+            ),
         }),
     ],
 });
 
-logger.debug('error');
+export { logger };
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,10 @@
 import app from './app';
 import { Config } from './config/index';
+import { logger } from './config/logger';
 
 const startServer = () => {
     app.listen(Config.PORT, () => {
-        console.log(`Server is running on port ${Config.PORT}...`);
+        logger.info(`Server is running on port ${Config.PORT}...`);
     });
 };
 
